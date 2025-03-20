f3a25d714da599685c2c0e6f6e33b7a91a495246
HellCC <61671551+HellC-Song@users.noreply.github.com>
2025-03-20 00:43:15 -0400

    Update HellCC's FRU script(HellCC的绝伊甸脚本).cs

--- FutureRewrittenUltimate/HellCC's FRU script(HellCC的绝伊甸脚本).cs
+++ FutureRewrittenUltimate/HellCC's FRU script(HellCC的绝伊甸脚本).cs
@@ -61,7 +61,7 @@ namespace HellCCKodakkuAssist.FutureRewrittenUltimate
          [ScriptMethod(name: "Phase1 Stack Range Of Turn Of The Heavens 光轮召唤分摊标记",
             eventType: EventTypeEnum.StartCasting,
             eventCondition: ["ActionId:regex:^(40152)$"])]
-        public void Phase1_Stack_Range_Of_Turn_Of_The_Heavens_光轮召唤分摊标记(Event @event, ScriptAccessory accessory)
+        """public void Phase1_Stack_Range_Of_Turn_Of_The_Heavens_光轮召唤分摊标记(Event @event, ScriptAccessory accessory)
         {
             int highPriorityTarget = P1转轮召抓人.IndexOf(1);
             int lowPriorityTarget = P1转轮召抓人.LastIndexOf(1);
@@ -77,5 +77,6 @@ namespace HellCCKodakkuAssist.FutureRewrittenUltimate
                 }
             }
         }
+        """
     }
 }
